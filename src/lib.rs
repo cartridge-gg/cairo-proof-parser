b78f2faef232db7328a48785ea885d700a31b0ef
Neo <neotheprogramist@proton.me>
2024-02-14 17:03:56 +0100

    add raw parse

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,8 @@ pub fn parse(input: String) -> anyhow::Result<Exprs> {
     let stark_proof = StarkProof::try_from(proof_json)?;
     Ok(Exprs::from(stark_proof))
 }
+
+pub fn raw_parse(input: String) -> anyhow::Result<StarkProof> {
+    let proof_json = serde_json::from_str::<ProofJSON>(&input)?;
+    StarkProof::try_from(proof_json)
+}
