23523e1f0e31f6e6aa8f9c2ee978225fb88c09c7
Neo <neotheprogramist@proton.me>
2024-03-28 21:39:30 +0100

    proof parse outputs

--- scripts/get_program_hash.py
+++ scripts/get_program_hash.py
@@ -6,8 +6,8 @@ from starkware.cairo.common.poseidon_hash import poseidon_hash_many
 def main():
     with open("proof.json", "r") as f:
         proof = load(f)
-    output_begin_addr = proof["public_input"]["memory_segments"]["output"]["begin_addr"]
-    output_stop_ptr = proof["public_input"]["memory_segments"]["output"]["stop_ptr"]
+    program_begin_addr = proof["public_input"]["memory_segments"]["program"]["begin_addr"]
+    program_stop_ptr = proof["public_input"]["memory_segments"]["program"]["stop_ptr"]
 
     public_memory =  proof["public_input"]["public_memory"]
     result = {}
@@ -16,12 +16,11 @@ def main():
         value = x["value"]
         result[address] = value
 
-    output = [int(result[i], 16) for i in range(output_begin_addr, output_stop_ptr)]
+    output = [int(result[i], 16) for i in range(program_begin_addr, program_stop_ptr)]
     print(output)
-
+    
     hash = poseidon_hash_many(output)
     print(hash)
-
 
 if __name__ == "__main__":
     main()
